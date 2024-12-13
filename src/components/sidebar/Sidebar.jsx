7f0ad07d2596438b48ce3ff38de0ac0027f3365e
IacheLopes <iachelopes1@gmail.com>
2024-12-13 13:46:16 -0300

    Adicionado todos os svgs da sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,12 @@
 import styles from "./sidebar.module.css";
 import lobbyNinjaLogo from "../../assets/lobbyNinjaLogo.svg";
 import favourites from "../../assets/favourites.svg";
+import tornauments from "../../assets/tournaments.svg";
+import planner from "../../assets/planner.svg";
+import registered from "../../assets/registered.svg";
+import skipped from "../../assets/skipped.svg";
+import alarm from "../../assets/alarm.svg";
+import deleted from "../../assets/deleted.svg";
 
 const Sidebar = () => {
   return (
@@ -9,17 +15,16 @@ const Sidebar = () => {
         <img src={lobbyNinjaLogo} />
       </div>
 
-
-      <div className={styles.bgFirstLi}>
-          <a href="#section1">
-           
-            
-          </a>
-        </div>
-
       <ul className="">
+        <li className={styles.bgFirstLi}>
+          <a href="#section1">
+            <img src={tornauments} alt="Tournaments" />
+          </a>
+        </li>
         <li>
-          <a href="#section2" className={styles.marginCorrection}>2</a>
+          <a href="#section2">
+            <img src={planner} alt="Planner" />
+          </a>
         </li>
         <li>
           <a href="#section3">
@@ -27,16 +32,24 @@ const Sidebar = () => {
           </a>
         </li>
         <li>
-          <a href="#section4">4</a>
+          <a href="#section4">
+            <img src={registered} alt="Registered" />
+          </a>
         </li>
         <li>
-          <a href="#section5">5</a>
+          <a href="#section5">
+            <img src={skipped} alt="Skipped" />
+          </a>
         </li>
         <li>
-          <a href="#section6">6</a>
+          <a href="#section6">
+            <img src={alarm} alt="Alarm" />
+          </a>
         </li>
         <li>
-          <a href="#section7">7</a>
+          <a href="#section7">
+            <img src={deleted} alt="Deleted" />
+          </a>
         </li>
       </ul>
     </div>
