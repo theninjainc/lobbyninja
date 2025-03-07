c50fc7c72d459d6b56004d425d9286479fb55231
Emerson Barbosa <emersin7x@MacBook-Pro-de-Emerson.local>
2025-03-07 01:28:51 -0300

    master: Testando alarme 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'; // Seu componente principal
 import './index.css';
 
+// Registrar o Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js') // Registro do service worker
+    .then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch(error => {
+      console.log('Erro ao registrar o Service Worker:', error);
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
